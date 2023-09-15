4b5094bfecd8b590a1cab1b2360c1c83729975b7
Kasia Strzelecka <katarzyna.anna.strzelecka@gmail.com>
2023-09-15 13:19:06 +0200

    skrypt i selektor w js

--- main.js
+++ main.js
@@ -5,3 +5,7 @@ const age = "37"
 
 console.log(`Nazywam się ${firstName} i mam ${age}lat`);
     
+const header = document.querySelector('.main__title-js')
+
+header.innerHTML = 'Tytuł ze skryptu!'
+
