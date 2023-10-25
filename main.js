a47c674813cfb5a02e3ab8b2c658946819eb2c97
Kasia Strzelecka <katarzyna.anna.strzelecka@gmail.com>
2023-10-25 23:43:43 +0200

    powitanie js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 const firstName = "Kasia"
 const age = "37"
 
-console.log(`Nazywam się ${firstName} i mam ${age}lat`);
+console.log(`Tutaj Nazywam się ${firstName} i mam ${age}lat`);
     
 const header = document.querySelector('.main__title-js');
 
@@ -16,3 +16,22 @@ const title = document.querySelector('.section__title-js');
 if (title != null) {
   title.innerHTML = 'Tytuł tego akapitu jest zapisy w skrypcie js.'
 }
+
+const calculate = myNumber =>(myNumber+3)*7 
+ 
+const result = calculate (2);
+console.log(result)
+
+let y = {"sdasd": "aaa"};
+const x = 1;
+console.log(y.sdasd)
+
+function hello (imie, wiek) {
+  return 'hello ' + imie + wiek;
+}
+console.log(hello('Kasia',37))
+
+console.log(hello('Dorka',37))
+
+const hello2 = (imie,wiek) => (`Witaj, nazywa się ${imie} i mam ${wiek}lat`)
+console.log (hello2(firstName,age))
